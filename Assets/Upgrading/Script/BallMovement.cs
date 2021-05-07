6eb2c083194a35e536dd695e267fb7838890bb34
Kuroake Vizago <zaebiyt@gmail.com>
2021-05-07 11:49:46 +0700

    Init Commit

---
+++ Assets/Upgrading/Script/BallMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    
+    [Header("Config : ")]
+    [SerializeField] float speed = 45f;
+    [SerializeField] float jumpForce = 350f;
+    [SerializeField] Rigidbody rigidbody;
+
+    bool isGrounded = true;
+
+    void Start()
+    {
+        if (!rigidbody)
+            rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+
+        Movement();
+
+        Jump();
+
+    }
+
+    void Movement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, 0);
+
+        rigidbody.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (isGrounded)
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isGrounded = false;
+                rigidbody.velocity = rigidbody.velocity + (Vector3.up * jumpForce);
+                //rigidbody.AddForce(new Vector3(0, jumpForce * Time.deltaTime, 0), ForceMode.Impulse);
+            }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = true;
+    }
+
+}
---
+++ Assets/Upgrading/Script/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header("Config : ")]
+    [SerializeField] GameObject followObject;
+
+    Vector3 offset;
+
+    private void Start()
+    {
+        if (!followObject)
+            followObject = GameObject.FindGameObjectWithTag("Player");
+        
+        offset = followObject.transform.position - transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = followObject.transform.position - offset;
+    }
+}
