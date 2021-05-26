2b98d3048850320e4f493272b52846ff7d5b4430
Kuroake Vizago <zaebiyt@gmail.com>
2021-05-26 14:27:22 +0700

    Major Update 3

---
+++ Assets/Upgrading/Script/FallBox.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallBox : MonoBehaviour
+{
+    [Header("Config : ")]
+    [SerializeField] GameObject playerGameObject;
+
+    Vector3 playerInitPosition;
+
+    private void Start()
+    {
+        if (playerGameObject)
+            playerInitPosition = playerGameObject.transform.position;
+        else
+        {
+            playerGameObject = GameObject.FindGameObjectWithTag("Player");
+            playerInitPosition = playerGameObject.transform.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.transform.position = playerInitPosition;
+            other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+
+}
---
+++ Assets/Upgrading/Script/WinBox.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinBox : MonoBehaviour
+{
+    [Header("Config : ")]
+    [SerializeField] GameObject victoryBanner;
+    [SerializeField] Text collectedCoinsText;
+    [SerializeField] BallMovement ballPlayer;
+
+    private void Start()
+    {
+        if (victoryBanner)
+            victoryBanner.SetActive(false);
+
+        if (!ballPlayer)
+            ballPlayer = GameObject.FindObjectOfType<BallMovement>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (collectedCoinsText)
+                collectedCoinsText.text = ballPlayer.coinCollected + " Coins Collected";
+            
+            victoryBanner.SetActive(true);
+
+
+
+        }
+    }
+
+
+}
